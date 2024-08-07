5764ba6502a0f3246e44a578a0469a0cce635262
pplateena <ukrstar38@gmail.com>
2024-08-07 14:20:24 +0300

    LC Assesment 929. Unique Email Addresses
    String manipulation, and hashset utilization for unique values extraction

---
+++ LC_easy/929. Unique Email Addresses_INT.py
@@ -0,0 +1,63 @@
+class Solution:
+    def numUniqueEmails(self, emails: list[str]) -> int:
+        # split @
+
+        actual_emails = set()
+
+        for email in emails:
+            if "@" not in email:
+                continue
+
+            working_parts = email.split('@')
+            prefix, suffix = working_parts[0], working_parts[1]
+
+            prefixFinished = []
+            for letter in prefix:
+                # if . del
+                # if + del
+                if letter == ".":
+                    continue
+                elif letter == "+":
+                    break
+                else:
+                    prefixFinished.append(letter)
+            prefixFinished = str(prefixFinished)
+
+            actualEmail = prefixFinished + "@" + suffix
+
+            actual_emails.add(actualEmail)
+        return len(actual_emails)
+
+        # add set final email
+        # return len(set)
+
+
+if __name__ == "__main__":
+    s = Solution()
+    assert s.numUniqueEmails(["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]) == 2
+    assert s.numUniqueEmails(["a@leetcode.com","b@leetcode.com","c@leetcode.com"]) == 3
+
+
+"""
+Every valid email consists of a local name and a domain name, separated by the '@' sign. Besides lowercase letters, the email may contain one or more '.' or '+'.
+
+For example, in "alice@leetcode.com", "alice" is the local name, and "leetcode.com" is the domain name.
+If you add periods '.' between some characters in the local name part of an email address, mail sent there will be forwarded to the same address without dots in the local name. Note that this rule does not apply to domain names.
+
+For example, "alice.z@leetcode.com" and "alicez@leetcode.com" forward to the same email address.
+If you add a plus '+' in the local name, everything after the first plus sign will be ignored. This allows certain emails to be filtered. Note that this rule does not apply to domain names.
+
+For example, "m.y+name@email.com" will be forwarded to "my@email.com".
+It is possible to use both of these rules at the same time.
+
+Given an array of strings emails where we send one email to each emails[i], return the number of different addresses that actually receive mails.
+
+Example 1:
+Input: emails = ["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]
+Output: 2
+Explanation: "testemail@leetcode.com" and "testemail@lee.tcode.com" actually receive mails.
+
+Example 2:
+Input: emails = ["a@leetcode.com","b@leetcode.com","c@leetcode.com"]
+Output: 3
+"""
