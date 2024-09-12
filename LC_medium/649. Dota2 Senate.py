4ae0c0bbb754a40e735944c37802f04468e3db95
pplateena <ukrstar38@gmail.com>
2024-09-12 22:30:19 +0300

    LC 649. Dota2 Senate
    Queue

---
+++ LC_medium/649. Dota2 Senate.py
@@ -0,0 +1,69 @@
+from collections import deque
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        r_q = deque()
+        d_q = deque()
+        n = len(senate)
+        for i in range(n):
+            if senate[i] == 'R':
+                r_q.append(i)
+            else:
+                d_q.append(i)
+        while len(r_q) != 0 and len(d_q) != 0:
+            r = r_q.popleft()
+            d = d_q.popleft()
+            n += 1
+            if r < d:
+                r_q.append(n)
+            else:
+                d_q.append(n)
+        if len(r_q) > 0:
+            return "Radiant"
+        return "Dire"
+
+
+if __name__ == "__main__":
+    s = Solution()
+    assert s.predictPartyVictory(senate = "RD") == "Radiant"
+    assert s.predictPartyVictory(senate = "RDD") == "Dire"
+
+"""
+In the world of Dota2, there are two parties: the Radiant and the Dire.
+The Dota2 senate consists of senators coming from two parties. Now the Senate wants to decide on a change in the Dota2 
+game. The voting for this change is a round-based procedure. In each round, each senator can exercise one of the two 
+rights:
+Ban one senator's right: A senator can make another senator lose all his rights in this and all the following rounds.
+Announce the victory: If this senator found the senators who still have rights to vote are all from the same party, he 
+can announce the victory and decide on the change in the game.
+Given a string senate representing each senator's party belonging. The character 'R' and 'D' represent the Radiant party 
+and the Dire party. Then if there are n senators, the size of the given string will be n.
+The round-based procedure starts from the first senator to the last senator in the given order. This procedure will last 
+until the end of voting. All the senators who have lost their rights will be skipped during the procedure.
+Suppose every senator is smart enough and will play the best strategy for his own party. Predict which party will 
+finally announce the victory and change the Dota2 game. The output should be "Radiant" or "Dire".
+
+ 
+
+Example 1:
+Input: senate = "RD"
+Output: "Radiant"
+Explanation: 
+The first senator comes from Radiant and he can just ban the next senator's right in round 1. 
+And the second senator can't exercise any rights anymore since his right has been banned. 
+And in round 2, the first senator can just announce the victory since he is the only guy in the senate who can vote.
+
+Example 2:
+Input: senate = "RDD"
+Output: "Dire"
+Explanation: 
+The first senator comes from Radiant and he can just ban the next senator's right in round 1. 
+And the second senator can't exercise any rights anymore since his right has been banned. 
+And the third senator comes from Dire and he can ban the first senator's right in round 1. 
+And in round 2, the third senator can just announce the victory since he is the only guy in the senate who can vote.
+ 
+
+Constraints:
+n == senate.length
+1 <= n <= 104
+senate[i] is either 'R' or 'D'.
+"""
