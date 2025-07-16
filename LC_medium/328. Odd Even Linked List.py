7223132f03d92c5fe58d8580d58412b627a37aac
pplateena <ukrstar38@gmail.com>
2025-07-16 17:32:27 +0300

    # Definition for singly-linked list.

---
+++ LC_medium/328. Odd Even Linked List.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # if head == None:
+        #     return None
+        if head == None:
+            return None
+        elif head.next == None:
+            return head
+
+        odd = head
+        first_even = even = head.next
+
+        while even and even.next:
+            odd.next, even.next = odd.next.next, even.next.next
+            odd, even = odd.next, even.next
+
+        odd.next = first_even
+        return head
+
+
+
+
